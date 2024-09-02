9f3a4e799a87bb14e92294bf27e30cec1a69e00d
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-09-03 00:04:54 +0300

    remove billing form

--- functions.php
+++ functions.php
@@ -292,26 +292,26 @@ function your_theme_setup()
 }
 add_action('after_setup_theme', 'your_theme_setup');
 
-// Hook in
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+// // Hook in
+// add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
-// Our hooked in function - $fields is passed via the filter!
-function custom_override_checkout_fields( $fields ) {
-    echo '<pre>'; print_r($fields); echo '</pre>';
-    unset( $fields['billing']['billing_first_name']);
-    unset( $fields['billing']['billing_last_name']);
-    unset( $fields['billing']['billing_company']);
-    unset( $fields['billing']['billing_address_1']);
-    unset( $fields['billing']['billing_address_2']);
-    unset( $fields['billing']['billing_city']);
-    unset( $fields['billing']['billing_postcode']);
-    unset( $fields['billing']['billing_country']);
-    unset( $fields['billing']['billing_state']);
-    unset( $fields['billing']['billing_email']);
-    unset( $fields['billing']['billing_phone']);
-    unset( $fields['order']['order_comments'] );
-    echo 'test remove billing';
-    return $fields;
-}
+// // Our hooked in function - $fields is passed via the filter!
+// function custom_override_checkout_fields( $fields ) {
+//     echo '<pre>'; print_r($fields); echo '</pre>';
+//     unset( $fields['billing']['billing_first_name']);
+//     unset( $fields['billing']['billing_last_name']);
+//     unset( $fields['billing']['billing_company']);
+//     unset( $fields['billing']['billing_address_1']);
+//     unset( $fields['billing']['billing_address_2']);
+//     unset( $fields['billing']['billing_city']);
+//     unset( $fields['billing']['billing_postcode']);
+//     unset( $fields['billing']['billing_country']);
+//     unset( $fields['billing']['billing_state']);
+//     unset( $fields['billing']['billing_email']);
+//     unset( $fields['billing']['billing_phone']);
+//     unset( $fields['order']['order_comments'] );
+//     echo 'test remove billing';
+//     return $fields;
+// }
 
-add_filter( 'woocommerce_cart_needs_shipping', '__return_false' );
+// add_filter( 'woocommerce_cart_needs_shipping', '__return_false' );
