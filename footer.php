04408fbce9a4579eeca6238a04f86563f4c889d3
Newbiez1 <jeangarodk@gmail.com>
2024-09-03 16:23:43 +0300

    bfuwef

--- footer.php
+++ footer.php
@@ -13,27 +13,26 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="footer-widgets">
-        <div class="container">
-            <div class="row">
-                <!-- Footer Widget Area 1 -->
-                <div>
-                    <!-- <?php if (is_active_sidebar('footer-1')): ?>
+    <div class="container">
+        <div class="row">
+            <!-- Footer Widget Area 1 -->
+            <div>
+                <!-- <?php if (is_active_sidebar('footer-1')): ?>
                         <?php dynamic_sidebar('footer-1'); ?>
                     <?php endif; ?> -->
-                </div>
-                <div class="col-4 location-widget">
-                    <h4>Location</h4>
-                    <p>
-                        <img src="/inc/assets/images/pngs/location.png" alt="Location Icon"
-                            style="vertical-align: middle; margin-right: 5px;">
-                        Dekweneh, George Matta St.,<br>
-                        Semitian Bldg, bloc A, 1st Floor.
-                    </p>
-                </div>
+            </div>
+            <div class="col-4 location-widget">
+                <h4>Location</h4>
+                <p>
+                    <img src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/location.png"
+                        alt="Location Icon" style="vertical-align: middle; margin-right: 5px;">
+                    Dekweneh, George Matta St.,<br>
+                    Semitian Bldg, bloc A, 1st Floor.
+                </p>
             </div>
         </div>
     </div>
+
 
 
 </footer><!-- #colophon -->
