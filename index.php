d74c92e922dcd22f43f459b8ddc5b5beccee82a4
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-04 12:07:44 +0300

    office-4-sept-14 (#59)

--- index.php
+++ index.php
@@ -13,10 +13,19 @@ while (have_posts()):
 endwhile;
 ?>
 
-<div class="homeDiv d-flex flex-column">
-    <div class="homeDivInnerWhite"></div>
-    <div class="homeDivInnerBlue"></div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="homeDiv">
+            <div class="col-10">
+                <div class="homeDivInnerWhite"></div>
+            </div>
+            <div class="col-2">
+                <div class="homeDivInnerBlue"></div>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <?php
 get_footer();
