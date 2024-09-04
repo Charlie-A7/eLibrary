89801772bdc2adc7bb80c5526178f9fc1999a3fa
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-04 12:57:01 +0300

    office-4-sept-22 (#70)

--- index.php
+++ index.php
@@ -17,7 +17,13 @@ endwhile;
     <div class="row h-100">
         <div class="col-3 homeDivInnerBlue my-3"></div>
         <div class="col-3 homeDivInnerWhite"></div>
-        <div class="col-5 homeDivInnerImage"></div>
+        <div class="col-5">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10 homeDivInnerImage"></div>
+                <div class="col-1"></div>
+            </div>
+        </div>
         <div class="col-1 homeDivInnerBlue"></div>
     </div>
 </div>
