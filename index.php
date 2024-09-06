0fbac1bf3b7744ba2123845b0455de0d079d70f4
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-06 11:08:23 +0300

    office-6-sept-2

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ endwhile;
         <div class="col-6 position-relative p-0">
             <div class="overlapping-content">
                     <h1>Welcome to our resource library</h1>
-                    <h4>Browse our selection, and order you need.</h4>
+                    <h4>Browse our selection, and order what you need.</h4>
             </div>
             <div class="row h-100">
                 <div class="col-5 homeDivInnerBlue my-3"></div>
