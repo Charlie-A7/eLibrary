5dd8c9ce50378e180b47500908e4ca9b76ba9730
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-09-09 23:06:13 +0300

    change basic page.php file

--- page.php
+++ page.php
@@ -14,19 +14,19 @@
 
 get_header(); ?>
 
-<div class="container">
+<div class="container-fluid">
 	<div class="row">
 		<div class="col-12">
 			<?php
 			while (have_posts()):
 				the_post();
 				the_content();
-				get_template_part('template-parts/content', 'page');
+				// get_template_part('template-parts/content', 'page');
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if (comments_open() || get_comments_number()):
-					comments_template();
-				endif;
+				// if (comments_open() || get_comments_number()):
+				// 	comments_template();
+				// endif;
 
 			endwhile; // End of the loop.
 			?>
