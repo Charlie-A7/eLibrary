ae5da8e6e73a7d9ecd75b53b16d83aaf09ee54d7
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-09 11:49:04 +0300

    office-9-sept-4 (#118)

--- functions.php
+++ functions.php
@@ -292,27 +292,8 @@ function your_theme_setup()
 }
 add_action('after_setup_theme', 'your_theme_setup');
 
-// Hook in
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields', 10, 1 );
 
-// Our hooked in function - $fields is passed via the filter!
-function custom_override_checkout_fields( $fields ) {
-    unset( $fields['billing']['billing_first_name']);
-    unset( $fields['billing']['billing_last_name']);
-    unset( $fields['billing']['billing_company']);
-    unset( $fields['billing']['billing_address_1']);
-    unset( $fields['billing']['billing_address_2']);
-    unset( $fields['billing']['billing_city']);
-    unset( $fields['billing']['billing_postcode']);
-    unset( $fields['billing']['billing_country']);
-    unset( $fields['billing']['billing_state']);
-    unset( $fields['billing']['billing_email']);
-    unset( $fields['billing']['billing_phone']);
-    unset( $fields['order']['order_comments'] );
-    return $fields;
-}
 
-// add_filter( 'woocommerce_cart_needs_shipping', '__return_false' );
 
 
 
