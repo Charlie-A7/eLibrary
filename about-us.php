694800afd403b8fab1bda87887efe7f140e58aa2
Newbiez1 <jeangarodk@gmail.com>
2024-09-10 14:51:30 +0300

    about us

--- about-us.php
+++ about-us.php
@@ -11,16 +11,15 @@ get_header();
 
         .homeDivInnerBlue {
             background-color: var(--Light-Gray-Blue-Color);
-            height: 100%;
         }
     }
 </style>
 
 <div class="container-fluid aboutUsDiv py-5">
-    <div class="row">
+    <div class="row h-100">
         <div class="col-1 homeDivInnerBlue p-2"></div>
 
-        <div class="col-7">
+        <div class="col-7 h-100">
             <img style="width: 100%; height: 40%"
                 src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/HoldingHands.jpg">
             <p>LifeAgape Lebanon is a Christian organization that works with all churches and Christian denominations.
@@ -30,6 +29,7 @@ get_header();
                 Gospel everywhere.</p>
             <p><strong>Our motto:</strong> Win, build and send multiplying disciples to fulfill the Great Commission.
             </p>
+            <button>العربية</button>
         </div>
 
 
