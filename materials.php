991ba26f361862eadd7ce344b4d68ee883dfa428
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-10 11:42:58 +0300

    9-sept-24 (#176)

--- materials.php
+++ materials.php
@@ -7,58 +7,24 @@ get_header();
 ?>
 
 <div class="container-fluid">
-    <!-- First Row: Custom Div and Limited Products -->
-    <div class="row">
-        <div class="col-3">
-            <p>Discover the bookshelf</p>
-        </div>
-        
-        <div class="col-9">
-            <?php
-            // Custom query for the first set of products
-            $args = array(
-                'post_type' => 'product',
-                'posts_per_page' => 3,
-                'product_cat' => 'materials',
-                'columns' => 3
-            );
-            $loop = new WP_Query($args);
-            if ($loop->have_posts()) :
-                echo '<ul class="products">';
-                while ($loop->have_posts()) : $loop->the_post();
-                    wc_get_template_part('content', 'product');
-                endwhile;
-                echo '</ul>';
-            endif;
-            wp_reset_postdata();
-            ?>
-        </div>
-    </div>
-
-    <!-- Second Row: Larger Product Grid -->
-    <div class="row">
-        <div class="col-12">
-            <?php
-            // Custom query for the second set of products
-            $args = array(
-                'post_type' => 'product',
-                'posts_per_page' => 16,
-                'product_cat' => 'materials',
-                'columns' => 4
-            );
-            $loop = new WP_Query($args);
-            if ($loop->have_posts()) :
-                echo '<ul class="products">';
-                while ($loop->have_posts()) : $loop->the_post();
-                    wc_get_template_part('content', 'product');
-                endwhile;
-                echo '</ul>';
-            endif;
-            wp_reset_postdata();
-            ?>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-3">
+			<p>Discover the bookshelf</p>
+		</div>
+		
+		<div class="col-9">
+			<?php
+			echo do_shortcode('[products category="materials" limit="3" columns="3"]');
+			?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<?php
+			echo do_shortcode('[products category="materials" limit="16" columns="4"]');
+			?>
+		</div>
+	</div>
 </div>
-
 <?php
 get_footer();
