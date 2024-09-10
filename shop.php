49c956ad8019ee50957f84a287842470a46ef75a
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-10 09:35:48 +0300

    10-sept-0 (#152)

---
+++ shop.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Shop
+ */
+
+get_header();
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<?php
+			while (have_posts()):
+				the_post();
+				the_content();
+				// get_template_part('template-parts/content', 'page');
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				// if (comments_open() || get_comments_number()):
+				// 	comments_template();
+				// endif;
+
+			endwhile; // End of the loop.
+			?>
+		</div>
+	</div>
+</div>
+<?php
+get_footer();
