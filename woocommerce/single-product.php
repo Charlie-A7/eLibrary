a14597151a51b3975154f840d85e5ad99c9a3840
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-10 10:53:21 +0300

    9-sept-15 (#167)

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -20,31 +20,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header( 'shop' ); ?>
-<div class="container">
-	<div class="row justify-content-center">
-        <?php
-            /**
-             * woocommerce_before_main_content hook.
-             *
-             * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-             * @hooked woocommerce_breadcrumb - 20
-             */
-            do_action( 'woocommerce_before_main_content' );
-        ?>
-    </div>
-    <div class="row px-0">
-        <?php while ( have_posts() ) : ?>
-            <?php the_post(); ?>
-            <?php wc_get_template_part( 'content', 'single-product' ); ?>
-        <?php endwhile; // end of the loop. ?>
-    </div>
+
+	<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+		<?php while ( have_posts() ) : ?>
+			<?php the_post(); ?>
+
+			<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
 	<?php
 		/**
 		 * woocommerce_after_main_content hook.
 		 *
 		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 		 */
-		// do_action( 'woocommerce_after_main_content' );
+		do_action( 'woocommerce_after_main_content' );
 	?>
 
 	<?php
@@ -53,9 +53,10 @@ get_header( 'shop' ); ?>
 		 *
 		 * @hooked woocommerce_get_sidebar - 10
 		 */
-		// do_action( 'woocommerce_sidebar' );
+		do_action( 'woocommerce_sidebar' );
 	?>
-</div>
+
 <?php
 get_footer( 'shop' );
+
 /* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
