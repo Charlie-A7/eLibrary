c7172d20170674577c6113e97387a46050eaff9e
Newbiez1 <jeangarodk@gmail.com>
2024-09-10 14:35:45 +0300

    about us

--- about-us.php
+++ about-us.php
@@ -11,16 +11,17 @@ get_header();
 
         .homeDivInnerBlue {
             background-color: var(--Light-Gray-Blue-Color);
+            height: 100%;
         }
     }
 </style>
 
 <div class="container-fluid aboutUsDiv py-5">
     <div class="row">
-        <div class="col-1 homeDivInnerBlue p-5"></div>
+        <div class="col-1 homeDivInnerBlue p-2"></div>
 
         <div class="col-7">
-            <img style="width: 100%;"
+            <img style="width: 100%; height: 40%"
                 src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/HoldingHands.jpg">
 
         </div>
