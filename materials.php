9707bdcdc7e8ce5cb796e2ca9b0f904633ea003b
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-10 20:00:51 +0300

    9-sept-34

--- materials.php
+++ materials.php
@@ -9,8 +9,8 @@ get_header();
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-3">
-				<h1>Discover</h1>
-				<h1>the bookshelf</h1>
+				<h1 class="vertical-text">Discover</h1>
+				<h1 class="vertical-text">the bookshelf</h1>
 			</div>
 			
 			<div class="col-9">
