e139d280d23800f431f6d6b922b5d0adf973365f
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-10 11:27:21 +0300

    9-sept-20 (#172)

--- materials.php
+++ materials.php
@@ -8,16 +8,14 @@ get_header();
 
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-12">
-            <div class="col-3">
-                <p>Discover the bookshelf</p>
-            </div>
-            
-			<div class="col-9">
-				<?php
-				echo do_shortcode('[products category="materials" limit="20" columns="4"]');
-				?>
-			</div>
+		<div class="col-3">
+			<p>Discover the bookshelf</p>
+		</div>
+		
+		<div class="col-9">
+			<?php
+			echo do_shortcode('[products category="materials" limit="20" columns="4"]');
+			?>
 		</div>
 	</div>
 </div>
