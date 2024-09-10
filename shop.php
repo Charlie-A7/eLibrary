18878cb63f98fb1236183a3e9843e26077402627
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-10 10:23:54 +0300

    10-sept-9

--- shop.php
+++ shop.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Shop
+ * Template Name: Materials
  */
 
 get_header();
@@ -11,19 +11,7 @@ get_header();
 		<div class="col-12">
             
 			<?php
-            //woocommerce_content();
             echo do_shortcode('[products]');
-			while (have_posts()):
-				the_post();
-				the_content();
-				// get_template_part('template-parts/content', 'page');
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				// if (comments_open() || get_comments_number()):
-				// 	comments_template();
-				// endif;
-
-			endwhile; // End of the loop.
 			?>
 		</div>
 	</div>
