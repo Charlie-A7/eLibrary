57165a8cbc17a36d12f2323722772d6fee153bc6
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-10 19:53:16 +0300

    9-sept-32

--- materials.php
+++ materials.php
@@ -5,23 +5,24 @@
 
 get_header();
 ?>
-
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-3">
-			<p>Discover the bookshelf</p>
-		</div>
-		
-		<div class="col-9">
-			<?php
-			echo do_shortcode('[products category="materials" limit="3" columns="3"]');
-			?>
-		</div>
-		
-		<div class="col-12">
-			<?php
-			echo do_shortcode('[products category="materials" limit="16" columns="4"]');
-			?>
+<div class="materials">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-3">
+				<h1>Discover the bookshelf</h1>
+			</div>
+			
+			<div class="col-9">
+				<?php
+				echo do_shortcode('[products category="materials" limit="3" columns="3"]');
+				?>
+			</div>
+			
+			<div class="col-12">
+				<?php
+				echo do_shortcode('[products category="materials" limit="16" columns="4"]');
+				?>
+			</div>
 		</div>
 	</div>
 </div>
