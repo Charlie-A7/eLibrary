de19a294b3a6a1f0ba7767026f70bfd12166291f
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-11 19:48:01 +0300

    11-sept-74

--- materials.php
+++ materials.php
@@ -8,7 +8,7 @@ get_header();
 <div class="materials py-5">
 	<div class="container-fluid">
 		<div class="row d-flex justify-content-end py-3">
-			<div class="col-3 text-right">
+			<div class="col-3 d-flex justify-content-end align-items-center">
 				<form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
 					<input type="hidden" name="post_type" value="product" /> <!-- Limit search to products -->
 					<input type="search" class="search-field" placeholder="Search products..." value="<?php echo get_search_query(); ?>" name="s" />
