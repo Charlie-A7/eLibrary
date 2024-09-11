ff86b216b4ed80decc8e4e02db9bce39fdaef3d7
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-11 17:14:41 +0300

    11-sept-36

--- materials.php
+++ materials.php
@@ -13,42 +13,42 @@ get_header();
 </style>
 <div class="materials">
 	<div class="container-fluid">
-			<?php
-				// Custom query for WooCommerce products in the "materials" category (limit 3)
-				$args1 = array(
-					'post_type' => 'product',
-					'posts_per_page' => 19,
-					'tax_query' => array(
-						array(
-							'taxonomy' => 'product_cat',
-							'field' => 'slug',
-							'terms' => 'materials',
-						),
+		<?php
+			// Custom query for WooCommerce products in the "materials" category (limit 3)
+			$args1 = array(
+				'post_type' => 'product',
+				'posts_per_page' => 19,
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'product_cat',
+						'field' => 'slug',
+						'terms' => 'materials',
 					),
-				);
+				),
+			);
 
-				$products_query1 = new WP_Query($args1);
+			$products_query1 = new WP_Query($args1);
 
-				if ($products_query1->have_posts()) : ?>
-					<div class="row">	
-						<div class="col-3 d-flex justify-content-center pb-2">
-							<h1>Discover the<br>bookshelf</h1>
-						</div>
-
-						<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
-							<div class="col-3 text-center">
-								<a href="<?php the_permalink(); ?>">
-									<?php the_post_thumbnail('medium'); ?>
-									<br>
-									<?php woocommerce_template_loop_add_to_cart(); ?>
-								</a>
-							</div>
-						<?php endwhile; ?>
+			if ($products_query1->have_posts()) : ?>
+				<div class="row">	
+					<div class="col-3 d-flex justify-content-center pb-2">
+						<h1>Discover the<br>bookshelf</h1>
 					</div>
-					<?php wp_reset_postdata(); ?>
-				<?php else : ?>
-					<p>No products found in the "materials" category.</p>
-				<?php endif; ?>
+
+					<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
+						<div class="col-3 text-center">
+							<a href="<?php the_permalink(); ?>">
+								<?php the_post_thumbnail('medium'); ?>
+								<br>
+								<?php woocommerce_template_loop_add_to_cart(); ?>
+							</a>
+						</div>
+					<?php endwhile; ?>
+				</div>
+				<?php wp_reset_postdata(); ?>
+			<?php else : ?>
+				<p>No products found in the "materials" category.</p>
+			<?php endif; ?>
 	</div>
 </div>
 <?php
