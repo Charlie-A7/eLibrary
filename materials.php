5f53f5841e728f5c26bc5a2a8b46e96fca067541
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-11 19:22:51 +0300

    11-sept-68

--- materials.php
+++ materials.php
@@ -10,28 +10,13 @@ get_header();
 		<div class="row d-flex justify-content-end py-3">
     		<div class="col-3">
         		<!-- WooCommerce Product Search Bar -->
-				<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<div>
-						<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search products..." />
-						<input type="hidden" name="post_type" value="product" />
-						<button type="submit" id="searchsubmit" class="btn btn-primary">Search</button>
-					</div>
-				</form>
+				<?php get_product_search_form(); ?>
         
         		<!-- Filter Button -->
         		<button type="button" class="btn btn-secondary ml-2" data-toggle="modal" data-target="#filterModal">
             		Filter
         		</button>
     		</div>
-		</div>
-
-		<div class="row">	
-			<div class="col-3 d-flex justify-content-center pb-3">
-				<h1 class="down-to-up">Discover the<br>bookshelf<br><span class="">..........</span></h1>
-			</div>
-			<div class="col-3 d-flex">
-				<?php get_search_form(); ?>
-			</div>
 		</div>
 
 		<?php
