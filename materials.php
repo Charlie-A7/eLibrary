426b291c6c4ccfb789570a348d5b08dc5c3cbcc4
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-11 18:15:01 +0300

    11-sept-48

--- materials.php
+++ materials.php
@@ -38,7 +38,7 @@ get_header();
 					<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
 						<div class="col-3 text-center pb-3 d-flex flex-column align-items-center">
 							<a href="<?php the_permalink(); ?>">
-								<?php the_post_thumbnail('medium'); ?>
+								<?php the_post_thumbnail('large'); ?>
 								<br>
 								<?php woocommerce_template_loop_add_to_cart(); ?>
 							</a>
