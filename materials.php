4c6d09098cb0af904829df491e1cd59515fb0c0d
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-11 14:14:00 +0300

    11-sept-21

--- materials.php
+++ materials.php
@@ -7,11 +7,6 @@ get_header();
 ?>
 <div class="materials">
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col-3 d-flex justify-content-center pb-2">
-				<h1>Discover the<br>bookshelf</h1>
-			</div>
-
 			<?php
 				// Custom query for WooCommerce products in the "materials" category (limit 3)
 				$args1 = array(
@@ -29,7 +24,11 @@ get_header();
 				$products_query1 = new WP_Query($args1);
 
 				if ($products_query1->have_posts()) : ?>
-					<div class="row">
+					<div class="row">	
+						<div class="col-3 d-flex justify-content-center pb-2">
+							<h1>Discover the<br>bookshelf</h1>
+						</div>
+
 						<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
 							<div class="col-3">
 								<a href="<?php the_permalink(); ?>">
@@ -44,17 +43,6 @@ get_header();
 				<?php else : ?>
 					<p>No products found in the "materials" category.</p>
 				<?php endif; ?>
-
-				<?php
-				// echo do_shortcode('[products category="materials" limit="3" columns="3"]');
-				?>
-			
-			<!-- <div class="col-12">
-				<?php
-				// echo do_shortcode('[products category="materials" limit="16" columns="4"]');
-				?>
-			</div> -->
-		</div>
 	</div>
 </div>
 <?php
