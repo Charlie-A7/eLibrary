c9d892d19dc7f6acd90d00dd6f265054e858b1c0
Newbiez1 <jeangarodk@gmail.com>
2024-09-12 15:12:11 +0300

    about sharnage3

--- about-us.php
+++ about-us.php
@@ -10,44 +10,41 @@ get_header();
 
 </style>
 
-<div class="aboutUsDiv py-5">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-1 aboutUsDivInnerBlue p-2"></div>
+<div class="aboutUsDiv py-5 position-relative">
 
-            <div class="col-11 px-3 position-relative">
-                <div class="row">
-                    <div class="AboutUs-Overlap">
-                        <h1>
-                            LifeAgape <br>
-                            حياة المحبة
-                        </h1>
-                    </div>
-                    <div class="col-7">
-                        <img class="AbtUSimg1"
-                            src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/HoldingHands.jpg">
-                        <p class="AboutUsText py-3">
-                            LifeAgape Lebanon is a Christian organization that works with all churches and
-                            Christian denominations. <br>
-                            Our goal is to deliver the message of Jesus Christ for a better life for every person through
-                            the
-                            various programs and activities that we carry out in different segments of society. Our mission:
-                            Spreading the
-                            Gospel everywhere.</p>
-                        <p><strong>Our motto:</strong> Win, build and send multiplying disciples to fulfill the Great
-                            Commission.
-                        </p>
-                        <button class="LanguageSwitchBtn">العربية</button>
-                    </div>
-                    <div class="col-4">
-                        <img class="AbtUSimg2"
-                            src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/trees.jpg">
-                    </div>
-                    <div class="col-1 aboutUsDivInnerBlue2 p-2"></div>
-                </div>
+    <div class="ABOUTUS-blue-left-sidebar position-absolute z-0"></div>
+    <div class="ABOUTUS-blue-right-sidebar position-absolute z-0"></div>
 
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="AboutUs-Overlap z-1 h-100 d-flex flex-column justify-content-center">
+                <h1>
+                    LifeAgape <br>
+                    حياة المحبة
+                </h1>
             </div>
-
+            <div class="col-7">
+                <img class="AbtUSimg1"
+                    src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/HoldingHands.jpg">
+                <p class="AboutUsText py-3">
+                    LifeAgape Lebanon is a Christian organization that works with all churches and
+                    Christian denominations. <br>
+                    Our goal is to deliver the message of Jesus Christ for a better life for every person
+                    through
+                    the
+                    various programs and activities that we carry out in different segments of society. Our
+                    mission:
+                    Spreading the
+                    Gospel everywhere.</p>
+                <p><strong>Our motto:</strong> Win, build and send multiplying disciples to fulfill the Great
+                    Commission.
+                </p>
+                <button class="LanguageSwitchBtn">العربية</button>
+            </div>
+            <div class="col-4">
+                <img class="AbtUSimg2"
+                    src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/trees.jpg">
+            </div>
         </div>
     </div>
 </div>
