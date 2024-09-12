02a27c8344062cb59f95086c590e7341de7d3533
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-12 14:00:55 +0300

    12-sept-38 (#349)

--- index.php
+++ index.php
@@ -19,8 +19,10 @@ get_header();
     <div class="container-fluid h-100">
         <div class="row h-100">
             <div class="col-6 z-1 h-100 d-flex flex-column justify-content-center">
-                <h1>Welcome to<br>our resource<br>library</h1>
-                <h4>Browse our selection, and order what you need.</h4>
+                <div class="padding-left">
+                    <h1>Welcome to<br>our resource<br>library</h1>
+                    <h4>Browse our selection, and order what you need.</h4>
+                </div>
             </div>
             <div class="col-6 z-1 h-100">
             </div>
