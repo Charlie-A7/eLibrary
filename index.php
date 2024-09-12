2a39138983364fe1b0305e48775f6daa6d76d5ef
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-12 13:35:55 +0300

    12-sept-31

--- index.php
+++ index.php
@@ -13,14 +13,16 @@ get_header();
 // endwhile;
 ?> 
 
-<div class="homeDiv py-5">
+<div class="homeDiv py-5  position-relative">
+    <div class="blue-left-sidebar position-absolute z-0"></div>
+    <div class="blue-right-sidebar position-absolute z-0"></div>
     <div class="container-fluid">
         <div class="row h-100">
-            <div class="col-6 position-relative p-0">
+            <div class="col-6 position-relative p-0 z-1">
                 <h1>Welcome to<br>our resource<br>library</h1>
                 <h4>Browse our selection, and order what you need.</h4>
             </div>
-            <div class="col-6">
+            <div class="col-6 z-1">
             </div>
         </div>
     </div>
