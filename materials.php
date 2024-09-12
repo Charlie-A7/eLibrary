2ff316f11aea01a0e1eaf395bbad51c1486fef9a
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-12 11:11:08 +0300

    12-sept-9 (#316)

--- materials.php
+++ materials.php
@@ -45,7 +45,7 @@ get_header();
 					</div>
 
 					<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
-						<div class="col-6 col-md-4 col-xl-3 text-center pb-3 d-flex flex-column align-items-center">
+						<div class="col-6 col-lg-4 col-xl-3 text-center pb-3 d-flex flex-column align-items-center">
 							<a href="<?php the_permalink(); ?>">
 								<?php the_post_thumbnail('medium'); ?>
 								<?php woocommerce_template_loop_add_to_cart(); ?>
