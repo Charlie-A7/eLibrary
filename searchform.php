14febe79356db5523793c3a2455d440ca1b4ce76
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-14 23:20:16 +0300

    14-sept-43 (#431)

--- searchform.php
+++ searchform.php
@@ -8,10 +8,28 @@
 
  -->
 
- <div class="search-container d-flex justify-content-center align-items-center">
-    <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" aria-label="Search for">
+ <!-- <div class="search-container d-flex justify-content-center align-items-center">
+    <form role="search" method="get" action="<?php // echo esc_url( home_url( '/' ) ); ?>" aria-label="Search for">
+        <input type="search" class="search-field" placeholder="Search..." value="<?php //echo get_search_query(); ?>" name="s">
+        <input type="hidden" name="cat" value="<?php //echo isset($_GET['cat']) ? esc_attr($_GET['cat']) : (is_page('materials') ? 'materials' : (is_page('library') ? 'library' : '')); ?>">
+        <button type="submit" class="search-button">
+            <i class="fa fa-search"></i>
+        </button>
+    </form>
+</div> -->
+
+
+<div class="search-container d-flex justify-content-center align-items-center">
+    <form role="search" method="get" action="<?php 
+        if (is_page('materials')) {
+            echo esc_url(get_permalink(get_page_by_path('materials')->ID));
+        } elseif (is_page('library')) {
+            echo esc_url(get_permalink(get_page_by_path('library')->ID));
+        } else {
+            echo esc_url(home_url('/'));
+        }
+    ?>" aria-label="Search for">
         <input type="search" class="search-field" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
-        <input type="hidden" name="cat" value="<?php echo isset($_GET['cat']) ? esc_attr($_GET['cat']) : (is_page('materials') ? 'materials' : (is_page('library') ? 'library' : '')); ?>">
         <button type="submit" class="search-button">
             <i class="fa fa-search"></i>
         </button>
