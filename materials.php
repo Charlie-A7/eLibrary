54bf0c3d999122f4485e7d2ef9f7ec5a047d5a90
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-14 20:14:24 +0300

    14-sept-1

--- materials.php
+++ materials.php
@@ -13,12 +13,13 @@ get_header();
 		<div class="row d-flex justify-content-end py-2">
 			<div class="col-6 col-lg-4 col-xl-3 d-flex justify-content-end align-items-center">
 				<div class="search-container">
-					<input type="search" class="search-field" placeholder="Search...">
-					<button type="submit" class="search-button">
-						<i class="fa fa-search"></i>
-					</button>
-				</div>
-
+					<form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<input type="search" class="search-field" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
+						<button type="submit" class="search-button">
+							<i class="fa fa-search"></i>
+						</button>
+					</form>
+				</div>	
 			</div>
 		</div>
 
