c24b0ea2880e2fe3d9c0f87504eefdc6fcd40c61
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-14 22:00:26 +0300

    14-sept-22

--- search.php
+++ search.php
@@ -25,53 +25,50 @@ get_header(); ?>
 			</div>
 		</div>
 	
+		<?php
+		// Define the search term
+		$search_term = isset($_GET['s']) ? sanitize_text_field($_GET['s']) : '';
 
-    <main id="main" class="site-main" role="main">
-        <?php
-        // Define the search term
-        $search_term = isset($_GET['s']) ? sanitize_text_field($_GET['s']) : '';
+		// Custom query for WooCommerce products in the "materials" category (limit 19)
+		$args1 = array(
+			'post_type' => 'product',
+			'posts_per_page' => 19,
+			's' => $search_term, // Ensure the search term is included
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'product_cat',
+					'field' => 'slug',
+					'terms' => 'materials',
+				),
+			),
+		);
 
-        // Custom query for WooCommerce products in the "materials" category (limit 19)
-        $args1 = array(
-            'post_type' => 'product',
-            'posts_per_page' => 19,
-            's' => $search_term, // Ensure the search term is included
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'product_cat',
-                    'field' => 'slug',
-                    'terms' => 'materials',
-                ),
-            ),
-        );
+		$products_query1 = new WP_Query($args1);
 
-        $products_query1 = new WP_Query($args1);
+		if ($products_query1->have_posts()) : ?>
+			<div class="row">
+				<div class="col-6 col-lg-4 col-xl-3 pb-3 z-1 d-flex justify-content-center">
+					<h1 class="down-to-up">
+						Discover the<br>bookshelf<br>
+						<span class="dots-design">..........</span>
+					</h1>
+				</div>
 
-        if ($products_query1->have_posts()) : ?>
-            <div class="row">
-                <div class="col-6 col-lg-4 col-xl-3 pb-3 z-1 d-flex justify-content-center">
-                    <h1 class="down-to-up">
-                        Discover the<br>bookshelf<br>
-                        <span class="dots-design">..........</span>
-                    </h1>
-                </div>
-
-                <?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
-                    <div class="col-6 col-lg-4 col-xl-3 text-center pb-3 d-flex flex-column align-items-center z-1">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('medium'); ?>
-                            <?php woocommerce_template_loop_add_to_cart(); ?>
-                        </a>
-                    </div>
-                <?php endwhile; ?>
-            </div>
-            <?php wp_reset_postdata(); ?>
-        <?php else : ?>
-            <p>No products found in the "materials" category with the search term "<?php echo esc_html($search_term); ?>".</p>
-        <?php endif; ?>
-		
-    </main><!-- #main -->
-</section><!-- #primary -->
+				<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
+					<div class="col-6 col-lg-4 col-xl-3 text-center pb-3 d-flex flex-column align-items-center z-1">
+						<a href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail('medium'); ?>
+							<?php woocommerce_template_loop_add_to_cart(); ?>
+						</a>
+					</div>
+				<?php endwhile; ?>
+			</div>
+			<?php wp_reset_postdata(); ?>
+		<?php else : ?>
+			<p>No products found in the "materials" category with the search term "<?php echo esc_html($search_term); ?>".</p>
+		<?php endif; ?>
+	</div>
+</div>
 
 <?php
 get_footer();
