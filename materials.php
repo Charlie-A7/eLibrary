20a9fe5b9f78980938814ea9c4f009c66f920f06
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-14 20:21:17 +0300

    14-sept-2

--- materials.php
+++ materials.php
@@ -6,62 +6,56 @@
 get_header();
 ?>
 <div class="materials py-2 position-relative">
-	<!-- sidebar blue bg -->
-	<div class="blue-sidebar position-absolute z-0"></div>
-	
-	<div class="container-fluid">
-		<div class="row d-flex justify-content-end py-2">
-			<div class="col-6 col-lg-4 col-xl-3 d-flex justify-content-end align-items-center">
-				<div class="search-container">
-					<form role="search" method="get" action="<?php echo esc_url( home_url( '/materials/' ) ); ?>">
-						<input type="search" class="search-field" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
-						<button type="submit" class="search-button">
-							<i class="fa fa-search"></i>
-						</button>
-					</form>
-				</div>	
-			</div>
-		</div>
+    <!-- sidebar blue bg -->
+    <div class="blue-sidebar position-absolute z-0"></div>
+    
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-end py-2">
+            <div class="col-6 col-lg-4 col-xl-3 d-flex justify-content-end align-items-center">
+                <div class="search-container">
+                    <form role="search" method="get" action="<?php echo esc_url( home_url( '/materials/' ) ); ?>">
+                        <input type="search" class="search-field" placeholder="Search..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+                        <button type="submit" class="search-button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-		<?php
-			// Custom query for WooCommerce products in the "materials" category (limit 3)
-			$args1 = array(
-				'post_type' => 'product',
-				'posts_per_page' => 19,
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'product_cat',
-						'field' => 'slug',
-						'terms' => 'materials',
-					),
-				),
-			);
+        <?php
+        // Custom query to search posts or products based on the search term
+        $args = array(
+            'post_type' => 'product', // Change to 'post' or any other post type if needed
+            's' => get_search_query(), // Get the search term
+            'posts_per_page' => 10,   // Limit number of posts
+        );
 
-			$products_query1 = new WP_Query($args1);
+        $custom_query = new WP_Query($args);
 
-			if ($products_query1->have_posts()) : ?>
-				<div class="row">	
-					<div class="col-6 col-lg-4 col-xl-3 pb-3 z-1 d-flex justify-content-center">
-						<h1 class="down-to-up">
-							Discover the<br>bookshelf<br>
-							<span class="dots-design">..........</span>
-						</h1>
-					</div>
+        if ($custom_query->have_posts()) : ?>
+            <div class="row">    
+                <div class="col-6 col-lg-4 col-xl-3 pb-3 z-1 d-flex justify-content-center">
+                    <h1 class="down-to-up">
+                        Discover the<br>bookshelf<br>
+                        <span class="dots-design">..........</span>
+                    </h1>
+                </div>
 
-					<?php while ($products_query1->have_posts()) : $products_query1->the_post(); ?>
-						<div class="col-6 col-lg-4 col-xl-3 text-center pb-3 d-flex flex-column align-items-center z-1">
-							<a href="<?php the_permalink(); ?>">
-								<?php the_post_thumbnail('medium'); ?>
-								<?php woocommerce_template_loop_add_to_cart(); ?>
-							</a>
-						</div>
-					<?php endwhile; ?>
-				</div>
-				<?php wp_reset_postdata(); ?>
-			<?php else : ?>
-				<p>No products found in the "materials" category.</p>
-			<?php endif; ?>
-	</div>
+                <?php while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
+                    <div class="col-6 col-lg-4 col-xl-3 text-center pb-3 d-flex flex-column align-items-center z-1">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('medium'); ?>
+                            <?php woocommerce_template_loop_add_to_cart(); ?>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+            <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+            <p>No products found for "<?php echo esc_html( get_search_query() ); ?>"</p>
+        <?php endif; ?>
+    </div>
 </div>
 <?php
 get_footer();
