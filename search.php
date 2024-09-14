609126194075897ffa905f6425e063ddde6277c5
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-14 21:46:04 +0300

    14-sept-20 (#408)

--- search.php
+++ search.php
@@ -10,6 +10,11 @@
 get_header(); ?>
 
 <section id="primary" class="content-area">
+
+	<header class="page-header">
+		<h1 class="page-title"><?php printf(esc_html__('Search Results for: %s', 'wp-bootstrap-starter'), '<span>' . get_search_query() . '</span>'); ?></h1>
+	</header><!-- .page-header -->
+
     <main id="main" class="site-main" role="main">
         <?php
         // Define the search term
@@ -53,28 +58,7 @@ get_header(); ?>
         <?php else : ?>
             <p>No products found in the "materials" category with the search term "<?php echo esc_html($search_term); ?>".</p>
         <?php endif; ?>
-
-        <?php
-        // Default search results for standard WordPress posts
-        if (have_posts()) : ?>
-
-            <header class="page-header">
-                <h1 class="page-title"><?php printf(esc_html__('Search Results for: %s', 'wp-bootstrap-starter'), '<span>' . get_search_query() . '</span>'); ?></h1>
-            </header><!-- .page-header -->
-
-            <?php
-            /* Start the Loop */
-            while (have_posts()) : the_post();
-                get_template_part('template-parts/content', 'search');
-            endwhile;
-
-            the_posts_navigation();
-
-        else :
-            get_template_part('template-parts/content', 'none');
-        endif;
-        ?>
-
+		
     </main><!-- #main -->
 </section><!-- #primary -->
 
