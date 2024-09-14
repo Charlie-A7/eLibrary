e9d0df7a3f21069199b4992ef0d55c9a9ddae546
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-14 21:04:27 +0300

    14-sept-11 (#399)

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<!-- <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label>
         <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'wp-bootstrap-starter' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'wp-bootstrap-starter' ); ?>">
     </label>
@@ -6,4 +6,11 @@
 </form>
 
 
+ -->
 
+ <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" aria-label="Search for">
+    <input type="search" class="search-field" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
+    <button type="submit" class="search-button">
+        <i class="fa fa-search"></i>
+    </button>
+</form>
