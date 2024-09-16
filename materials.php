51df4e8bf5bc9641e45d641a34319a864d323f9d
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2024-09-16 17:12:48 +0300

    16-sept-0 (#458)

--- materials.php
+++ materials.php
@@ -11,7 +11,7 @@ get_header();
 	
 	<div class="container-fluid">
 		<div class="row d-flex justify-content-end py-2">
-			<div class="col-6 col-lg-4 col-xl-3 d-flex justify-content-end align-items-center">
+			<div class="col-12 d-flex justify-content-end align-items-center">
 				<?php get_template_part('searchform'); ?>
 			</div>
 		</div>
