664ad553e45b553fe7e2f48db9839bafedaa4851
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-16 17:40:23 +0300

    16-sept-4

--- searchform.php
+++ searchform.php
@@ -18,20 +18,44 @@
     </form>
 </div>
 <div>
-    <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <!-- <form method="get" action="<?php// echo esc_url( home_url( '/' ) ); ?>">
         <label for="category" class="filter-label">
-            <i class="fas fa-filter"></i> <!-- FontAwesome filter icon -->
+            <i class="fas fa-filter"></i>
         </label>
         <?php
-            wp_dropdown_categories( array(
-                'show_option_none' => '',
-                'name' => 'category',
-                'orderby' => 'name',
-                'hierarchical' => true,
-                'selected' => get_query_var( 'cat' ),
-                'taxonomy' => 'product_cat' // Use 'product_cat' for WooCommerce product categories
-            ) );
+            // wp_dropdown_categories( array(
+            //     'show_option_none' => '',
+            //     'name' => 'category',
+            //     'orderby' => 'name',
+            //     'hierarchical' => true,
+            //     'selected' => get_query_var( 'cat' ),
+            //     'taxonomy' => 'product_cat' // Use 'product_cat' for WooCommerce product categories
+            // ) );
         ?>
+        <input type="submit" value="Filter">
+    </form> -->
+
+    <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <select name="category" id="category">
+            <option value="">
+                <label for="category" class="filter-label">
+                    <i class="fas fa-filter"></i> <!-- FontAwesome filter icon -->
+                </label>
+            </option>
+            <?php
+                $terms = get_terms( array(
+                    'taxonomy' => 'product_cat',
+                    'orderby' => 'name',
+                    'hide_empty' => false, // Show empty categories as well
+                ) );
+
+                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+                    foreach ( $terms as $term ) {
+                        echo '<option value="' . esc_attr( $term->slug ) . '">' . esc_html( $term->name ) . '</option>';
+                    }
+                }
+            ?>
+        </select>
         <input type="submit" value="Filter">
     </form>
 </div>
