33f65f76155ca4608f5ae3ca896a75fa9f865103
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-17 13:25:18 +0300

    17-sept-1

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ get_header();
             </div>
             <div class="col-6 z-1 h-100 d-flex flex-column justify-content-center">
                 <div class="padding-left">
-                    <h1>مرحباً بكم في<br>مكتبة الموارد<br>الخاصة بنا.</h1>
-                    <h4>تصفح مجموعتنا، واطلب ما تحتاجه.</h4>
+                    <h1 class="rtl">مرحباً بكم في<br>مكتبة الموارد<br>الخاصة بنا.</h1>
+                    <h4 class="rtl">تصفح مجموعتنا، واطلب ما تحتاجه.</h4>
                 </div>
             </div>
             <div class="col-6 z-1 h-100">
