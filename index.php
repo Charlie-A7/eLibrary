06556972c64dd13486991fd2ccf5f32152d8f626
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-17 14:35:52 +0300

    17-sept-8

--- index.php
+++ index.php
@@ -12,11 +12,13 @@ get_header();
     <div class="container-fluid h-100 position-relative">
         <div class="row h-100">
             <div class="col-6 z-1 h-100 d-flex flex-column justify-content-center">
-                <div class="padding-left">
-                    <h1 id="englishText" class="visible position-absolute">Welcome to<br>our resource<br>library</h1>
-                    <h4 id="englishSub" class="visible position-absolute">Browse our selection, and order what you need.</h4>
-                    <h1 id="arabicText" class="hidden rtl arabic-medium-font position-absolute">مرحباً بكم في<br>مكتبة الموارد<br>الخاصة بنا.</h1>
-                    <h4 id="arabicSub" class="hidden rtl arabic-regular-font position-absolute">تصفح مجموعتنا، واطلب ما تحتاجه.</h4>
+                <div id="englishText-index" class="padding-left position-absolute visible">
+                    <h1>Welcome to<br>our resource<br>library</h1>
+                    <h4>Browse our selection, and order what you need.</h4>
+                </div>
+                <div id="arabicText-index" class="padding-left position-absolute hidden">
+                    <h1 class=" rtl arabic-medium-font">مرحباً بكم في<br>مكتبة الموارد<br>الخاصة بنا.</h1>
+                    <h4 class="rtl arabic-regular-font">تصفح مجموعتنا، واطلب ما تحتاجه.</h4>
                 </div>
             </div>
             <div class="col-6 z-1 h-100">
@@ -30,10 +32,8 @@ get_header();
 get_footer();
 ?>
 <script>
-    const englishText = document.getElementById('englishText');
-    const englishSub = document.getElementById('englishSub');
-    const arabicText = document.getElementById('arabicText');
-    const arabicSub = document.getElementById('arabicSub');
+    const englishText = document.getElementById('englishText-index');
+    const arabicText = document.getElementById('arabicText-index');
 
     let isEnglishVisible = true;
 
@@ -42,43 +42,31 @@ get_footer();
             // Slide out English text
             englishText.classList.remove('visible');
             englishText.classList.add('slide-out');
-            englishSub.classList.remove('visible');
-            englishSub.classList.add('slide-out');
 
             // Slide in Arabic text after the English text slides out
             setTimeout(() => {
                 englishText.classList.add('hidden');
-                englishSub.classList.add('hidden');
 
                 arabicText.classList.remove('hidden');
-                arabicSub.classList.remove('hidden');
 
                 arabicText.classList.remove('slide-out');
-                arabicSub.classList.remove('slide-out');
 
                 arabicText.classList.add('slide-in');
-                arabicSub.classList.add('slide-in');
             }, 1000); // Match the duration of the slide-out animation
         } else {
             // Slide out Arabic text
             arabicText.classList.remove('slide-in');
             arabicText.classList.add('slide-out');
-            arabicSub.classList.remove('slide-in');
-            arabicSub.classList.add('slide-out');
 
             // Slide in English text after the Arabic text slides out
             setTimeout(() => {
                 arabicText.classList.add('hidden');
-                arabicSub.classList.add('hidden');
 
                 englishText.classList.remove('hidden');
-                englishSub.classList.remove('hidden');
 
                 englishText.classList.remove('slide-out');
-                englishSub.classList.remove('slide-out');
 
                 englishText.classList.add('slide-in');
-                englishSub.classList.add('slide-in');
             }, 1000); // Match the duration of the slide-out animation
         }
 
