b0320f526fc932e9719785489c3b9504b26c389f
Newbiez1 <jeangarodk@gmail.com>
2024-09-19 14:11:54 +0300

    ayo

--- my-cart.php
+++ my-cart.php
@@ -5,12 +5,12 @@
 
 get_header();
 ?>
-<div class="my-custom-cart-wrapper position-relative z-1">
+<div class="my-custom-cart-wrapper position-relative z-1 h-100">
 
     <div class="cart-lightblue-left-sidebar position-absolute z-2"></div>
     <div class="cart-darkblue-right-sidebar position-absolute z-2"></div>
 
-    <div class="container-fluid h-100">
+    <div class="container h-100">
         <div class="row">
             <!-- <div class="col-12">
                 <?php
