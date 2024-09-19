029c796e3f99f26e1828e52231f164e8eca48ec1
Newbiez1 <jeangarodk@gmail.com>
2024-09-19 13:57:49 +0300

    allons-y

--- my-cart.php
+++ my-cart.php
@@ -7,12 +7,11 @@ get_header();
 ?>
 <div class="my-custom-cart-wrapper position-relative z-1">
 
+    <div class="cart-lightblue-left-sidebar position-absolute z-2"></div>
+    <div class="cart-darkblue-right-sidebar position-absolute z-2"></div>
+
     <div class="container-fluid z-1 h-100">
         <div class="row z-1">
-
-            <div class="col-6 cart-lightblue-left-sidebar position-absolute z-2"></div>
-            <div class="col-6 cart-darkblue-right-sidebar position-absolute z-2"></div>
-
             <!-- <div class="col-12">
                 <?php
                 // Display WooCommerce Cart
@@ -23,18 +22,19 @@ get_header();
         <div class="row z-3">
             <div class="col-2"></div>
 
-            <div class="row z-3">
-                <div class="col-8">
-                    <h1>YOUR CART</h1>
-                    <div class="BooksInCart">
+            <div class="col-8">
+                <div class="row z-3">
+                    <div class="col-8">
+                        <h1>YOUR CART</h1>
+                        <div class="BooksInCart">
+
+                        </div>
+                    </div>
+                    <div class="row-4">
 
                     </div>
                 </div>
-                <div class="row-4">
-
-                </div>
             </div>
-
             <div class="col-2"></div>
         </div>
     </div>
