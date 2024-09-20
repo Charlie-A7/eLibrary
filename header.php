7ff950719c99fe55601990ee82b9d6f41e68b48b
Charlie-A7 <charlieaintablian@gmail.com>
2024-09-20 21:38:52 +0300

    20-sept-36

--- header.php
+++ header.php
@@ -83,15 +83,18 @@
         <div id="content" class="site-content">
 
 <script>
-    document.getElementById('hamburger-button').addEventListener('click', function() {
+    const hamburger =document.getElementById('hamburger-button');
+    hamburger.addEventListener('click', function() {
         const mobileNav = document.querySelector(".mobile-nav-wrapper");
         
         if (mobileNav.classList.contains('d-none')) {
             mobileNav.classList.remove('d-none');
             mobileNav.classList.add('d-block');
+            hamburger.classList.add('hamburger-active');
         } else {
             mobileNav.classList.add('d-none');
             mobileNav.classList.remove('d-block');
+            hamburger.classList.remove('hamburger-active');
         }
     });
 </script>
