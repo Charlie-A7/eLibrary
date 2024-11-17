30ee13a474e03e52ecc500ebdc0c6408f96c249e
Newbiez1 <jeangarodk@gmail.com>
2024-11-17 17:52:35 +0200

    singlepage3

--- single-product.php
+++ single-product.php
@@ -18,15 +18,13 @@ get_header(); ?>
 
             // Fetch custom fields (if using Advanced Custom Fields or similar plugins)
             $product_price = get_post_meta($product_id, '_price', true);
-            $product_description = $_product->get_description();
+            $product_description = get_post_meta($product_id, '_description', true);
             $product_image = get_the_post_thumbnail_url($product_id, 'full');
 
             // Output the product details
             ?>
-            <div class="single_product_details">
-                <div class="product-header">
-                    <h1><?php the_title(); ?></h1>
-                </div>
+            <div class="product-header">
+                <h1><?php the_title(); ?></h1>
                 <div class="product-image">
                     <?php if ($product_image): ?>
                         <img src="<?php echo esc_url($product_image); ?>" alt="<?php the_title_attribute(); ?>">
@@ -35,18 +33,17 @@ get_header(); ?>
                 <div class="product-price">
                     <span><?php echo esc_html($product_price); ?></span>
                 </div>
-
-                <div class="product-description">
-                    <?php echo wp_kses_post($product_description); ?>
-                </div>
-                <?php
+            </div>
+            <div class="product-description">
+                <?php echo wp_kses_post($product_description); ?>
+            </div>
+            <?php
 
         endwhile;
     else:
         echo '<p>No product found.</p>';
     endif;
     ?>
-    </div>
 
 </div>
 
