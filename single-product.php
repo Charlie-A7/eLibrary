5fda55ea8ef3dd4ed549265a03a401985af0390b
Newbiez1 <jeangarodk@gmail.com>
2024-11-17 18:02:53 +0200

    singlepage3

--- single-product.php
+++ single-product.php
@@ -17,23 +17,20 @@ get_header(); ?>
             $product_id = get_the_ID();
 
             // Fetch custom fields (if using Advanced Custom Fields or similar plugins)
-            $product_price = get_post_meta($product_id, '_price', true);
-            $product_description = get_post_meta($product_id, '_description', true);
+            $product_description = get_post_field('post_content', $product_id);
             $product_image = get_the_post_thumbnail_url($product_id, 'full');
 
             // Output the product details
             ?>
             <div class="product-header">
                 <h1><?php the_title(); ?></h1>
-                <div class="product-image">
-                    <?php if ($product_image): ?>
-                        <img src="<?php echo esc_url($product_image); ?>" alt="<?php the_title_attribute(); ?>">
-                    <?php endif; ?>
-                </div>
-                <div class="product-price">
-                    <span><?php echo esc_html($product_price); ?></span>
-                </div>
             </div>
+            <div class="product-image">
+                <?php if ($product_image): ?>
+                    <img src="<?php echo esc_url($product_image); ?>" alt="<?php the_title_attribute(); ?>">
+                <?php endif; ?>
+            </div>
+
             <div class="product-description">
                 <?php echo wp_kses_post($product_description); ?>
             </div>
