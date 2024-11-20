a7cf0461b00af8752aa096280499d6c43ec8c826
Newbiez1 <jeangarodk@gmail.com>
2024-11-20 14:40:59 +0200

    separating spaces

--- single-product.php
+++ single-product.php
@@ -6,51 +6,48 @@
 get_header(); ?>
 
 <div class="container-fluid single-product-container position-relative">
-    <div class="row">
-        <div class="singleProduct-blue-left-sidebar position-absolute col-1"></div>
-        <div class="singleProduct-blue-right-sidebar position-absolute col-1"></div>
 
-        <?php
-        // Check if it's a single product page
-        if (have_posts()):
-            while (have_posts()):
-                the_post();
+    <?php
+    // Check if it's a single product page
+    if (have_posts()):
+        while (have_posts()):
+            the_post();
 
-                // Get the product ID
-                $product_id = get_the_ID();
+            // Get the product ID
+            $product_id = get_the_ID();
 
-                // Fetch custom fields (if using Advanced Custom Fields or similar plugins)
-                $product_description = get_post_field('post_content', $product_id);
-                $product_image = get_the_post_thumbnail_url($product_id, 'full');
+            // Fetch custom fields (if using Advanced Custom Fields or similar plugins)
+            $product_description = get_post_field('post_content', $product_id);
+            $product_image = get_the_post_thumbnail_url($product_id, 'full');
 
-                // Output the product details
-                ?>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-1"></div>
-                        <div class="product-header col-10">
-                            <h1><?php the_title(); ?></h1>
-                        </div>
-                        <div class="product-image col-4">
-                            <?php if ($product_image): ?>
-                                <img src="<?php echo esc_url($product_image); ?>" alt="<?php the_title_attribute(); ?>">
-                            <?php endif; ?>
-                        </div>
+            // Output the product details
+            ?>
+            <div class="single_product_container">
+                <div class="row">
+                    <div class="singleProduct-blue-left-sidebar col-1"></div>
+                    <div class="singleProduct-blue-right-sidebar col-1"></div>
 
-                        <div class="product-description col-5">
-                            <?php echo wp_kses_post($product_description); ?>
-                        </div>
-                        <div class="col-1"></div>
+                    <div class="product-header col-10">
+                        <h1><?php the_title(); ?></h1>
+                    </div>
+                    <div class="product-image col-4">
+                        <?php if ($product_image): ?>
+                            <img src="<?php echo esc_url($product_image); ?>" alt="<?php the_title_attribute(); ?>">
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="product-description col-5">
+                        <?php echo wp_kses_post($product_description); ?>
                     </div>
                 </div>
-                <?php
+            </div>
+            <?php
 
-            endwhile;
-        else:
-            echo '<p>No product found.</p>';
-        endif;
-        ?>
-    </div>
+        endwhile;
+    else:
+        echo '<p>No product found.</p>';
+    endif;
+    ?>
 </div>
 
 <?php get_footer(); ?>
