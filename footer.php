29176b113956fbb65a462597d1213978e02bcf8b
Newbiez1 <jeangarodk@gmail.com>
2024-11-21 22:40:26 +0200

    adjust-spacings-on-mobile

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
             <div class="col-12 col-sm-4 footer_lines location_section">
                 <h5 class="footer_h5">Location</h5>
                 <a href="https://maps.app.goo.gl/RX31MfYc181fp3X5A">
-                    <p>
+                    <p class="location-text-switch-mobile">
                         <img class="footer-icon1"
                             src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/location.png"
                             alt="Location Icon">
