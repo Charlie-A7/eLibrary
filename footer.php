dab87f3cb860e3f7f3989bb1397926f0d4a41a50
Newbiez1 <jeangarodk@gmail.com>
2024-11-22 17:01:55 +0200

    fix centering

--- footer.php
+++ footer.php
@@ -16,15 +16,16 @@
             <div class="col-12 col-sm-4 footer_lines location_section">
                 <p>
                     <span class="location-icon-mobile">
-                        <img class="footer-icon1-mobile"
-                            src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/location.png"
-                            alt="Location Icon">
+                        <p>
+                            <img class="footer-icon1-mobile"
+                                src="https://charlie.e-vents.me/wp-content/themes/main/inc/assets/images/location.png"
+                                alt="Location Icon">
                         <h5 class="footer_h5">Location</h5>
-                    </span>
-                <h5 class="footer_h5 remove-on-mobile">Location</h5>
-
-
                 </p>
+                </span>
+                <h5 class="footer_h5 remove-on-mobile">Location</h5>
+                </p>
+
                 <a href="https://maps.app.goo.gl/RX31MfYc181fp3X5A">
                     <p class="location-text-switch-mobile">
                         <img class="footer-icon1"
