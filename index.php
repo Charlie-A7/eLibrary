a98311c1fc0d58f49b6978e819a8ca6e9e8831c3
Charlie-A7 <152325317+Charlie-A7@users.noreply.github.com>
2025-01-30 20:31:19 +0200

    habibi charlie12 (#970)

--- index.php
+++ index.php
@@ -44,7 +44,11 @@ get_header();
             <div class="row row1 my-0">
                 <div class="col-12">
                     <!-- <div class="dots-design text-right">..........</div> -->
-                    <div class="dots-design-2"></div>
+                    <?php 
+                        for ($i = 0; $i < 10; $i++) {
+                            echo '<div class="dots-design-2"></div>';
+                        }
+                    ?>
                     <h1>Welcome to our<br>resource library</h1>
                     <h6>Browse our selection, and order what you need.</h6>
                 </div>
@@ -58,7 +62,12 @@ get_header();
             </div>
             <div class="row row3 my-0">
                 <div class="col-12">
-                    <!-- <div class="dots-design text-left">..........</div> -->
+                    <!-- <div class="dots-design text-left">..........</div> --><div id="dots-container">
+                    <?php 
+                        for ($i = 0; $i < 10; $i++) {
+                            echo '<div class="dots-design-2"></div>';
+                        }
+                    ?>
                     <h1 class=" rtl arabic-medium-font homepage-mobile-arabic-text-big">مرحباً بكم في مكتبة <br>الموارد الخاصة بنا.</h1>
                     <h6 class="rtl arabic-regular-font homepage-mobile-arabic-text-small">تصفح مجموعتنا، واطلب ما تحتاجه.</h6>
                 </div>
